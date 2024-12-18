61b49e3c9d9804e8bbe01defaf1b2195f4e25ca1
Malvin A <malvinaudriansyah77@gmail.com>
2024-12-18 14:15:12 +0700

    changing hero font size for mobile screen

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
 
         <div className="absolute w-full max-w-[1240px] px-20 left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-center lg:text-start lg:justify-start lg:items-start">
           <div className="w-full max-w-[600px]">
-            <h1 className="text-[122px] leading-[136px]">
+            <h1 className="text-[70px] md:text-[122px] md:leading-[136px]">
               READY TO TRAIN <span>YOUR BODY</span>
             </h1>
             <p className="mb-14">
