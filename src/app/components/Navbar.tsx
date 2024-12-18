852d2ec6e716619bba9468b3f1cd50c7d639caf2
Malvin A <malvinaudriansyah77@gmail.com>
2024-12-18 13:54:38 +0700

    responsive navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,7 +23,7 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`flex flex-row w-full max-w-[full] h-20 px-24 fixed top-0 z-50 transition-colors duration-700 ${
+        className={`flex flex-col w-full max-w-[full] justify-center items-center px-24 py-5 sticky top-0 z-50 transition-colors duration-700 ${
           scrolled ? "bg-[#121212]" : "bg-transparent"
         }`}
       >
@@ -25,7 +31,8 @@ export default function Navbar() {
           <h1 className="text-[32px]">
             NEXTGEN<span>GYM</span>
           </h1>
-          <div className="flex flex-row gap-10 text-[21px] text-white">
+          {/* Desktop Menu */}
+          <div className="hidden lg:flex flex-row gap-10 text-[21px] text-white">
             <h1 className="hover:text-[#ff0000] transition-all duration-900">
               HOME
             </h1>
@@ -42,8 +49,36 @@ export default function Navbar() {
               CONTACT US
             </h1>
           </div>
-          <h1 className="text-[21px] px-7 py-2 bg-[#ff0000] hover:bg-white hover:text-[#ff0000] transition-all duration-900">
+          <h1 className="hidden lg:flex text-[21px] px-7 py-2 bg-[#ff0000] hover:bg-white hover:text-[#ff0000] transition-all duration-900">
             START NOW
+          </h1>
+          <RxHamburgerMenu
+            onClick={handleOpen}
+            size={30}
+            className="lg:hidden"
+          />
+        </div>
+        <div
+          className={
+            isOpen
+              ? "flex flex-col w-full max-w-full text-center lg-hidden"
+              : "hidden"
+          }
+        >
+          <h1 className="hover:text-[#ff0000] transition-all duration-900">
+            HOME
+          </h1>
+          <h1 className="hover:text-[#ff0000] transition-all duration-900">
+            SERVICE
+          </h1>
+          <h1 className="hover:text-[#ff0000] transition-all duration-900">
+            TRAINERS
+          </h1>
+          <h1 className="hover:text-[#ff0000] transition-all duration-900">
+            TESTIMONIAL
+          </h1>
+          <h1 className="hover:text-[#ff0000] transition-all duration-900">
+            CONTACT US
           </h1>
         </div>
       </div>
